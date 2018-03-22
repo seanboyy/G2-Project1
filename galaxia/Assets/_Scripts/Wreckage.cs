a913d2b51559b5e34cf3f3e9623f83124b6af503
wytriam <thomas.m.frick@gmail.com>
2018-03-22 13:24:05 -0400

    started ship wreckage

---
+++ galaxia/Assets/_Scripts/Wreckage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wreckage : MonoBehaviour
+{
+    public GameObject[] parts;
+
+    IEnumerator breakApart()
+    {
+        // for each part in parts, set its direction in a random direction
+        // give each part a random velocity forward, from a range of speed + or - velRange
+        // destroy all pieces of the wreckage 5 seconds later
+        yield return null;
+    }
+}
