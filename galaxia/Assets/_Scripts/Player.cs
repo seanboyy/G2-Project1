923ae82e2d33a0f0c9cb47d4d5c514b9fcac66dc
wytriam <thomas.m.frick@gmail.com>
2018-03-22 11:58:35 -0400

    added a sandbox for myself, simplified the current sandbox

---
+++ galaxia/Assets/_Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float movement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector3 pos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(pos.x + movement, pos.y, pos.z);
+	}
+}
