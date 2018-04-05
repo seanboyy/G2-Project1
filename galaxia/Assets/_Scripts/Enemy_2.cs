8fc42f63b79e0aacf3d77e0006af071a750a4ca8
wytriam <thomas.m.frick@gmail.com>
2018-04-05 01:48:01 -0400

    updated prefabs, added laser

--- galaxia/Assets/_Scripts/Enemy_2.cs
+++ galaxia/Assets/_Scripts/Enemy_2.cs
@@ -7,10 +7,13 @@ public class Enemy_2 : Enemy_0
     [Header("Set Dynamically - Enemy_2")]
     public GameObject[] minions = new GameObject[4];
 
+    public GameObject particleLaserCharging;
+    public GameObject laser;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        laser.SetActive(false);
 	}
 
     public override void Move()
@@ -21,16 +24,23 @@ public class Enemy_2 : Enemy_0
                 // move back and forth around the top of the screen...ominously - follow the player
                 pos = new Vector3(Constants.instance.playerPos.x, pos.y, pos.z);
                 break;
-            case EnemyState.charging:
-                // grab nearby enemies and turn them into minions
-                break;
-            case EnemyState.attacking:
-                // fire a massive laser
-                break;
+            //case EnemyState.charging:
+            //    // grab nearby enemies and turn them into minions
+            //    break;
+            //case EnemyState.attacking:
+            //    // fire a massive laser
+            //    break;
             default:
                 // do nothing
                 break;
         }
+    }
+
+    IEnumerator Charging()
+    {
+        status = EnemyState.charging;
+        particleLaserCharging.GetComponent<ParticleSystem>().Play();
+        yield return null;
     }
 
 }
