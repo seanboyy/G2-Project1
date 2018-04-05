0214777f068b9951695ea39500a47a9e24faa519
wytriam <thomas.m.frick@gmail.com>
2018-04-04 22:18:28 -0400

    started wave message

---
+++ galaxia/Assets/_Scripts/WaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [Header("Enemies Spawned Bottom-Up")]
+    public GameObject[] enemies;
+
+    [Header("Set Dynamically")]
+    public int enemiesAlive = 0;
+
+    // Some constants to help make rows
+    private int e0pRow = 7;
+    private int e0spacing = 6; 
+    private int e1pRow = 5;
+    private int e1spacing = 0;
+    private int e2pRos = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void SpawnRank()
+    {
+
+    }
+}
